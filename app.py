ae02b62e0414b3d25c37c13c3b8752ba8221e09f
avneetwadhwa873@gmail.com <avneet1@ibm.com>
2024-07-29 14:23:25 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, make_response, jsonify
+from model import db
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('api/v1/products')
+def getAllProducts():
+    return make_response(jsonify({"products": db}), 200)
+
+
+if __name__ == '__main__':
+    app.run()
