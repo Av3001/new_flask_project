2476121a5c88e48cb83db8c656631ebd17e6b811
avneetwadhwa873@gmail.com <avneet1@ibm.com>
2024-07-30 12:39:16 +0530

    Revert "Hello world changed"
    This reverts commit 6722c30178fdbaefe3d2a46f1a1b582a2e277fcd.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'New Hello World!'
+    return 'Hello World!'
 
 
 @app.route('api/v1/products')
