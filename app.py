6722c30178fdbaefe3d2a46f1a1b582a2e277fcd
avneetwadhwa873@gmail.com <avneet1@ibm.com>
2024-07-30 12:38:31 +0530

    Hello world changed

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'New Hello World!'
 
 
 @app.route('api/v1/products')
