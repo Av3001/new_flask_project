e91825599ba5c1f5974d4bb3ce77aa4cd6e1e47c
avneetwadhwa873@gmail.com <avneet1@ibm.com>
2024-07-31 11:06:51 +0530

    app.py error

--- app.py
+++ app.py
@@ -9,11 +9,7 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
-<<<<<<< HEAD
 @app.route('/api/products')
-=======
-@app.route('/api/v1/products')
->>>>>>> 38619add5693761e3ca995cde765f9a67530344f
 def getAllProducts():
     return make_response(jsonify({"products": db}), 200)
 
